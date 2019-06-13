68f9784c6a82800162a7a2702ffd5ac1fa9291ab
nicolasperuch <nicolas.peruch@southsystem.com.br>
2019-06-13 15:50:27 -0300

    Adding swagger

--- src/main/java/com/github/nicolasperuch/api/VoteApi.java
+++ src/main/java/com/github/nicolasperuch/api/VoteApi.java
@@ -1,14 +1,18 @@
 package com.github.nicolasperuch.api;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
+@RequestMapping("vote")
 public class VoteApi {
 
+    @ApiOperation(value = "This is a hi endpoint!")
     @GetMapping
     public ResponseEntity<?> sayHi(){
         return ok("hi");
---
+++ src/main/java/com/github/nicolasperuch/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.github.nicolasperuch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket SwaggerVoteApi(){
+        return new Docket(SWAGGER_2)
+                    .select()
+                    .apis(basePackage("com.github.nicolasperuch.api"))
+                    .paths(regex(".*"))
+                    .build();
+    }
+}
